4c9e89647923fd5957bba0d69d13494ad34cff2a
chenyouwei3 <2148275751@qq.com>
2024-05-29 23:56:41 +0800

    class1

---
+++ class01/test.md
@@ -0,0 +1,2 @@
+//goctl api go -api user.api -dir ../  --style=goZero
+//goctl rpc protoc user.proto --go_out=../ --go-grpc_out=../  --zrpc_out=../ --style=goZero
---
+++ class01/user-api/internal/logic/userInfoLogic.go
@@ -0,0 +1,31 @@
+package logic
+
+import (
+	"context"
+
+	"class01/user-api/internal/svc"
+	"class01/user-api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UserInfoLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+// 获取用户信息
+func NewUserInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserInfoLogic {
+	return &UserInfoLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *UserInfoLogic) UserInfo(req *types.UserInfoReq) (resp *types.UserInfoResp, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}
